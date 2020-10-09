cbcdc8409ea4a0d331e3bc1941bc2df480d281a5
mkshgh <52674161+mkshgh@users.noreply.github.com>
2020-10-09 20:00:40 +0545

    .NET version and Registry Logging

--- _net_mail_notWorking.ps1
+++ _net_mail_notWorking.ps1
@@ -1,40 +1,61 @@
+Write-Host "##################################################################################"
+Write-Host "Copyright © 2020, [mkshgh](https://github.com/mkshgh)" -ForegroundColor Green
+Write-Host "##################################################################################"
+
 
 #This will be used to check which values are already set in the protocols
 #Returns the validated .net paths where the values need to be changed
 #reutrns the valid _net_path
-function _net_path_parser{
-
+function get_net_versions{
+    $reg_parent_path1 = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\.NETFramework"
+    #Get the .Net version Name using regex v[24] means v2 or v4 which searches these keys
+    $_net_path_array = (Get-ChildItem $reg_parent_path1).Name -match "v[1-9]"
+    $_version = foreach ($item in $_net_path_array) {
+         Split-Path $item -leaf
+    }
+    Write-Host $_version -ForegroundColor Green
 }
+
+
 function update_SystemDefaultTlsVersions_net {
 
+    #The parameters are here
     #The path to check if the .net versions are available or not
     $reg_parent_path1 = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\.NETFramework"
     $reg_parent_path2 = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\.NETFramework"
+    #Adding the property ("Name","PropertyType","Value") from this array
+    $property_param="SystemDefaultTlsVersions","DWord","00000001"
 
-    Write-Host "Enter the .net Versions you want to enable TLS 1.2 for"
-    Write-Host "Eg: if .NET2 and .NET4 write: 24"
+
+    #This prints the available versions in the system
+    Write-Host "You have the followig .NET versions available: "-NoNewline; get_net_versions
+    Write-Host "##################################################################################"
+    Write-Host""
+    Write-Host "Eg: if .NET2 and .NET4 write: "-ForegroundColor Yellow -NoNewline; Write-Host "24" -ForegroundColor Green;
+
+
     #Input the versions that you need to enable the TLS 1.2 in
-    $_net_versions = Read-Host ".net Versions" 
+    $_net_versions = Read-Host "Enter the .net Version to Enable TLS 1.2 on (Leave Empty if for all .NET versions)" ; 
 
-    #If empty set to check all the versions then use all the .net version from 1-9 though might not exist
-    if (!$_net_versions) {
-        $_net_versions = '[1-9]'
+    #Adds all the avialable versions to enable the TLS 1.2 in the computers
+    if ([string]::IsNullOrEmpty($_net_versions)) {
+        Write-Host "enteredhere"
+        #Although only upto .NETv4 is available. we use 1-9 for searching here, which is fine.
+        $_net_versions = '1-9'
+    }
+    elseif (!($_net_versions -match '[1-9]')) {
+        Write-Error 'The Versions can be only Decimal Numbers, alphabets or characters not allowed'
     }
 
-    #Get the child items of this Node
-    #pipeline the output to filter the name of the child nodes
-    $_net_paths = Get-ChildItem -Path $reg_parent_path1 | Select-Object
-    #array to store the validated paths for checking
-    
     #check if the item is the given .net version or not 
     #store the checked values to the given array
-    #If the it is not a .net version then add to an array v[24] means v2 or v4 which searches these keys
+    #If the it is not a .net version then add to an array 
+    #Get the .Net version Name using regex v[24] means v2 or v4 which searches these keys
     $_net_path_array = (Get-ChildItem $reg_parent_path1).Name -match "v[$_net_versions]"
     #add the reg_parent_path2 childItems to the list too
     $_net_path_array += (Get-ChildItem $reg_parent_path2).Name -match "v[$_net_versions]"
 
-    #Adding the property ("Name","PropertyType","Value") from this array
-    $property_param="SystemDefaultTlsVersions","DWord","00000001"
+
     #Adding the Regestry keys in the windows system
     foreach ($_net_path in $_net_path_array) {
         #Try to insert create a new property
@@ -55,9 +76,19 @@ function update_SystemDefaultTlsVersions_net {
 
         #show the updated values at the end.
         finally{
-            Get-ItemProperty -Path "Registry::$_net_path" 
+            Write-Host "At $_net_path"
+            Write-Host "Paremeter set to "-ForegroundColor Red -NoNewLine; 
+            Write-Host "$property_param" -ForegroundColor Green; 
         }
     }
 
 }
+
 update_SystemDefaultTlsVersions_net
+
+# # This is where I test the code
+# function Test {
+#   
+# }
+
+# Test
